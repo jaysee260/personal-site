f2bd5fa08673dc841733deacc4df5eb9cf4dbf58
Juan Carlos Gonzalez <jcgonzalezmolina@gmail.com>
2018-04-30 19:35:39 -0400

    testing slightly different layout for skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,11 +1,26 @@
 import { h } from 'preact';
 import data from './skills.json';
 import Skill from './Skill';
+import Category from './Category';
 
 // Render an icon for every skill
 function renderSkills(data) {
-  let skills = data.map(s => <Skill icon={s.icon} name={s.name} />)
-  return skills;
+  let categories = [];
+  data.forEach(skill => {
+    if(!categories.includes(skill.type))
+      categories.push(skill.type);
+  });
+
+  let sets = categories.map(type => {
+    // return skill whose type matches current type
+    let skills = data.filter(skill => skill.type === type);
+    return <Category type={type} skills={skills} />
+  })
+
+  return sets;
+
+  // let skills = data.map(s => <Skill icon={s.icon} name={s.name} />)
+  // return skills;
 }
 
 const Skillset = () => 
