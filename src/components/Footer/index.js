82da5b020e2c6bde08fb3c6657317795a9d4279c
Juan Carlos Gonzalez <jcgonzalezmolina@gmail.com>
2018-05-02 17:06:24 -0400

    wip commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+
+const Footer = () => 
+  <footer>
+    Footer
+    {/* <img src='./icons/github.svg' />
+    <img src='./icons/twitter.svg' />
+    <img src='./icons/email.svg' /> */}
+  </footer>
+
+export default Footer;
